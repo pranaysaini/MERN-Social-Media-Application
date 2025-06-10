99e437986a1ffba67500997f2fbded02ad4974e8
Pranay Saini <sainipranay36@gmail.com>
2025-06-10 12:04:57 +0530

    social-media-app

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -81,7 +81,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("api/auth/login", {
+    const loggedInResponse = await fetch("/api/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
