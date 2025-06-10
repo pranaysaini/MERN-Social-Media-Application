7519d8db32e9f0f23ec9ce4915302433d97ae881
Pranay Saini <sainipranay36@gmail.com>
2025-06-10 10:35:35 +0530

    social-media-frontend

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${userId}/friends`,
+      `https://mern-social-backend-v6np.onrender.com//users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
